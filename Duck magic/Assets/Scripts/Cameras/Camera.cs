80292f21078418c82fa71395570dabee40988333
NikitaTsekhanovich <Nikita.Tsekhanovich@urfu.me>
2024-03-24 22:14:46 +0500

    init TextMesh/ Refactoring scene

--- Duck magic/Assets/Scripts/Cameras/Camera.cs
+++ Duck magic/Assets/Scripts/Cameras/Camera.cs
@@ -4,16 +4,15 @@ namespace Cameras
 {
     public class Camera : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
+        [SerializeField] private Transform _player;
             
+        private void Update()
+        {
+            var cameraPosition = transform.position;
+            cameraPosition.x = _player.position.x;
+            cameraPosition.y = _player.position.y;
+
+            transform.position = cameraPosition;
         }
     }
 }
