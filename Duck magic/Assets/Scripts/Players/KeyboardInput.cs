4b9e055c084ce1fd5fb34d463007a3ff203e701c
NikitaTsekhanovich <Nikita.Tsekhanovich@urfu.me>
2024-03-25 12:36:18 +0500

    Fixed synchronized movement

--- Duck magic/Assets/Scripts/Players/KeyboardInput.cs
+++ Duck magic/Assets/Scripts/Players/KeyboardInput.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Photon.Pun;
 
 namespace Players
 {
@@ -9,22 +11,31 @@ namespace Players
         [SerializeField] private KeyCode _left;
         [SerializeField] private KeyCode _jump;
         [SerializeField] private KeyCode _attack;
-
         
+        private PhotonView _view;
+
+        private void Start()
+        {
+            _view = GetComponent<PhotonView>();
+        }
+
         private void Update()
         {
-            if (Input.GetKey(_right))
+            if (_view.IsMine)
             {
-                _movement.MoveRight();
-            }
-            else if (Input.GetKey(_left))
-            {
-                _movement.MoveLeft();
-            }
+                if (Input.GetKey(_right))
+                {
+                    _movement.MoveRight();
+                }
+                else if (Input.GetKey(_left))
+                {
+                    _movement.MoveLeft();
+                }
 
-            if (Input.GetKeyDown(_jump))
-            {
-                _movement.Jump();
+                if (Input.GetKeyDown(_jump))
+                {
+                    _movement.Jump();
+                }
             }
         }
     }
