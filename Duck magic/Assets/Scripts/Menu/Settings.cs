94485c692acaaaece643fedbb3a4bf18630552c6
NikitaTsekhanovich <Nikita.Tsekhanovich@urfu.me>
2024-06-18 22:34:12 +0500

    music settings, refactoring ui

--- Duck magic/Assets/Scripts/Menu/Settings.cs
+++ Duck magic/Assets/Scripts/Menu/Settings.cs
@@ -1,33 +1,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class Settings : MonoBehaviour
+namespace Menu
 {
-    public TMPro.TMP_Dropdown Dropdown;
-    private List<Resolution> Resolutions;
-    public Toggle Toggle;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Settings : MonoBehaviour
     {
-        Toggle.isOn = Screen.fullScreen;
-        Resolutions = Screen.resolutions.Distinct().ToList();
-        Resolutions.Reverse();
-        var res = Resolutions.Select(resolution => resolution.ToString()).ToList();
-        Dropdown.ClearOptions();
-        Dropdown.AddOptions(res);
-        Screen.SetResolution(Resolutions.First().width, Resolutions.First().height, Screen.fullScreen);
-    }
+        [SerializeField] private TMPro.TMP_Dropdown Dropdown;
+        [SerializeField] private Toggle Toggle;
+        [SerializeField] private AudioMixer _mixer;
+        [SerializeField] private Slider _musicSlider;
+        [SerializeField] private Slider _soundSlider;
+        private List<Resolution> Resolutions;
+        private string _musicMixerName = "MusicVol";
+        private string _soundMixerName = "SoundVol";
 
-    public void SetResolution()
-    {
-        Screen.SetResolution(Resolutions[Dropdown.value].width, Resolutions[Dropdown.value].height, Screen.fullScreen);
-    }
+        private const float _multiplier = 20f;
 
-    public void ChangeScreenMode()
-    {
-        Screen.fullScreen = Toggle.isOn;
+        private void Awake()
+        {
+            _musicSlider.onValueChanged.AddListener(HandleMusicSlider);
+            _soundSlider.onValueChanged.AddListener(HandleSoundSlider);
+        }
+
+        private void HandleMusicSlider(float value)
+        {
+            var volume = Mathf.Log10(value) * _multiplier;
+            _mixer.SetFloat(_musicMixerName, volume);
+        }
+
+        private void HandleSoundSlider(float value)
+        {
+            var volume = Mathf.Log10(value) * _multiplier;
+            _mixer.SetFloat(_soundMixerName, volume);
+        }
+
+        private void Start()
+        {
+            Toggle.isOn = Screen.fullScreen;
+            Resolutions = Screen.resolutions.Distinct().ToList();
+            Resolutions.Reverse();
+            var res = Resolutions.Select(resolution => resolution.ToString()).ToList();
+            Dropdown.ClearOptions();
+            Dropdown.AddOptions(res);
+            Screen.SetResolution(Resolutions.First().width, Resolutions.First().height, Screen.fullScreen);
+        }
+
+        public void SetResolution()
+        {
+            Screen.SetResolution(Resolutions[Dropdown.value].width, Resolutions[Dropdown.value].height, Screen.fullScreen);
+        }
+
+        public void ChangeScreenMode()
+        {
+            Screen.fullScreen = Toggle.isOn;
+        }
     }
 }
